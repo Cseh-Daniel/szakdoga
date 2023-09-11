d4b43f539c2f3857a24a453fa43d00a514579d33
Daniel Cseh <codaryf@gmail.com>
2023-09-11 11:43:56 +0200

    login, logout, register done

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -14,16 +14,21 @@
     </Head>
 
     <body>
-        <!-- -------------------------------------------------------------------- -->
-        <h3>nav</h3>
-        <div v-if="$page.props.auth.user == null" class="d-flex flex-row-reverse gap-3 p-2 align-items-center">
+        <!-- -----------------------------navbarba componentbe külön szedni--------------------------------------- -->
+        <div class="d-inline-flex flex-row w-50">
+            <h3>Student Jobs</h3>
+        </div>
+        <!-- login component? -->
+        <div v-if="$page.props.auth.user == null" class="d-inline-flex flex-row-reverse gap-3 p-2 align-items-center w-50">
             <Link href="/register">Regisztráció</Link>
             <Link href="/login" as="button" class="btn btn-primary">Bejelentkezés</Link>
         </div>
-        <div v-else class="d-flex flex-row-reverse gap-3 p-2 align-items-center">
+
+        <div v-else class="d-inline-flex flex-row-reverse gap-3 p-2 align-items-center w-50">
             <Link href="/logout" as="button" class="btn btn-outline-secondary" method="post">
-                Kijelentkezés
+            Kijelentkezés
             </Link>
+            <span class="border-end border-dark-subtle p-2">{{ $page.props.auth.user.username }}</span>
         </div>
 
         <!-- --------------------------------------------------------------- -->
