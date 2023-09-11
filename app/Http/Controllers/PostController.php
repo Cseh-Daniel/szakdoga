b7493f0328233ec6f6963fc444afddc1640f2a5b
Daniel Cseh <codaryf@gmail.com>
2023-09-11 12:15:01 +0200

    model, resource controller, migration

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the post.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new post.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created post in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified post.
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified post.
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified post in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified post from storage.
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
