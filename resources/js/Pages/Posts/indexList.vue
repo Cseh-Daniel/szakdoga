33048ee595bff6753003ab3aeb01935303bb376d
Cseh Dániel Kristóf <codaryf@gmail.com>
2023-10-11 11:33:26 +0200

    indexList component

---
+++ resources/js/Pages/Posts/indexList.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+const props= defineProps(['posts'])
+
+</script>
+
+<template>
+
+
+    <div class="d-flex flex-column justify-content-center shadow-sm w-90 p-3 m-auto rounded-3 border border-top-0">
+<!-- List element -->
+        <p class="align-text-bottom border-bottom p-2 rounded-2 shadow-sm fs-4" v-for="post in props.posts">
+        <div class="d-flex flex-row align-items-center justify-content-between">
+            <span class="fs-3">
+               <Link :href="'/posts/'+post.id">
+                {{ post.title }}
+               </Link>
+            </span>
+            <span class="fs-6">
+                {{ post.user.name }}
+            </span>
+        </div>
+
+        {{ post.text }}
+        </p>
+
+    </div>
+
+</template>
--- resources/js/Pages/index.vue
+++ resources/js/Pages/index.vue
@@ -22,29 +22,8 @@ import indexList from './Posts/indexList.vue';
     Bejegyzés írása
     </Link>
 
-<indexList posts='$page.props.posts'></indexList>
+<indexList :posts="$page.props.posts"></indexList>
 
-    <!-- kiemelni content list component-be -->
-
-    <!-- <div class="d-flex flex-column justify-content-center shadow-sm w-90 p-3 m-auto rounded-3 border border-top-0"> -->
-
-        <!-- List element component?? -->
-        <!-- <p class="align-text-bottom border-bottom p-2 rounded-2 shadow-sm fs-4" v-for="post in $page.props.posts">
-        <div class="d-flex flex-row align-items-center justify-content-between">
-            <span class="fs-3">
-                <Link :href="'/posts/' + post.id">
-                {{ post.title }}
-                </Link>
-            </span>
-            <span class="fs-6">
-                {{ post.user.name }}
-            </span>
-        </div>
-
-        {{ post.text }}
-        </p> -->
-
-    </div>
 </template>
 
 
