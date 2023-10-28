60814ef917d63b1c6836e04e80ed691405e8d13b
Cseh Dániel Kristóf <codaryf@gmail.com>
2023-10-28 13:02:13 +0200

    fix - misstyped 'role_id' on users table

--- database/migrations/2023_10_30_000000_create_users_table.php
+++ database/migrations/2023_10_30_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreignId('roles_id')->constrained()->onUpdate('cascade')->onDelete('cascade')->default(2);
+            $table->foreignId('role_id')->constrained()->onUpdate('cascade')->onDelete('cascade')->default(2);
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -74,13 +74,13 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@user.com',
-            'role_id' => 2,
+            'role_id' => 2
         ]);
 
         \App\Models\User::factory()->create([
             'name' => 'Admin User',
             'email' => 'admin@user.com',
-            'role_id' => 1,
+            'role_id' => 1
         ]);
     }
 }
